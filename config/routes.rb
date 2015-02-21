020ed703790a55bc1a3cafa5ac6bdf11d5a67cca
Sharon <sglangevin@gmail.com>
2015-02-21 13:30:19 -0800

    Allow people to vote

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
+  # You can have the root of your site routed with "root"
+  root 'topics#index'
+
+  resources :topics do
+    member do
+      post 'upvote'
+    end
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
